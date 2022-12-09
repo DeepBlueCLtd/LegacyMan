38a41bdbe8689f39f1c44a3650f47de287f0ee35
rnll <neomashab@gmail.com>
2022-12-09 16:49:24 +0530

    Update output message

--- legacyman_parser/process_regions.py
+++ legacyman_parser/process_regions.py
@@ -6,7 +6,7 @@ from crawler.simple_crawler import SimpleCrawler
 def process_soup(returned_soup: BeautifulSoup, url: str, parent_url: str):
     """The regions are processed from map"""
     for area_element in returned_soup.find_all('area'):
-        print(area_element.get('alt'))
+        print("{}  ==> {}".format(area_element.get('alt'), area_element.get('href')))
 
 
 craw = SimpleCrawler("data/PlatformData/PD_1.html", True)
