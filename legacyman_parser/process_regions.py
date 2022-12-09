12b9cdcc5ea8090ee86f3cf001b91d632d6d6ddf
rnll <neomashab@gmail.com>
2022-12-09 16:53:46 +0530

    Filter for special case - Britain

--- legacyman_parser/process_regions.py
+++ legacyman_parser/process_regions.py
@@ -6,7 +6,8 @@ from crawler.simple_crawler import SimpleCrawler
 def process_soup(returned_soup: BeautifulSoup, url: str, parent_url: str):
     """The regions are processed from map"""
     for area_element in returned_soup.find_all('area'):
-        print("{}  ==> {}".format(area_element.get('alt'), area_element.get('href')))
+        if not area_element.get('href').startswith('..'):
+            print("{}  ==> {}".format(area_element.get('alt'), area_element.get('href')))
 
 
 craw = SimpleCrawler("data/PlatformData/PD_1.html", True)
