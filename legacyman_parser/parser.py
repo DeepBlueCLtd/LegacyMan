cb1c372252bcb95fdf0337ae74ac46bf89247dbf
rnll <neomashab@gmail.com>
2022-12-10 09:18:13 +0530

    Collate countries without valid href

--- legacyman_parser/parser.py
+++ legacyman_parser/parser.py
@@ -5,6 +5,8 @@ from legacyman_parser.parse_countries import extract_countries_in_region, COUNTR
 from legacyman_parser.parse_regions import extract_regions, REGION_COLLECTION
 from legacyman_parser.parse_units_and_tonals_of_country import extract_units_tonals_of_country, UNIT_COLLECTION
 
+INVALID_COUNTRY_HREFS = []
+
 
 def parse_from_root():
     arg = sys.argv[0:]
@@ -14,8 +16,8 @@ def parse_from_root():
 
     """Parsing Region
     The regions are processed from map"""
-    spidey = SimpleCrawler(url=arg[1], disable_crawler_log=True)
-    spidey.crawl(resource_processor_callback=extract_regions, crawl_recursively=False)
+    root_spidey_to_extract_regions = SimpleCrawler(url=arg[1], disable_crawler_log=True)
+    root_spidey_to_extract_regions.crawl(resource_processor_callback=extract_regions, crawl_recursively=False)
 
     for region in REGION_COLLECTION:
         """Parsing Countries from extracted regions
@@ -23,21 +25,37 @@ def parse_from_root():
         if region.url.endswith('Britain1.html'):
             continue
         reg_dict = {"region": region.region}
-        region_spidey = SimpleCrawler(url=region.url, disable_crawler_log=True, userland_dict=reg_dict)
-        region_spidey.crawl(resource_processor_callback=extract_countries_in_region, crawl_recursively=False)
+        region_spidey_to_extract_countries = SimpleCrawler(url=region.url,
+                                                           disable_crawler_log=True,
+                                                           userland_dict=reg_dict)
+        region_spidey_to_extract_countries.crawl(resource_processor_callback=extract_countries_in_region,
+                                                 crawl_recursively=False)
         print("parser/Region: ", region)
 
     for country in COUNTRY_COLLECTION:
         """Parsing class in each country and their units"""
         print("parser/Country: ", country)
         if country.url is None:
+            INVALID_COUNTRY_HREFS.append({"country": country.country,
+                                         "url": country.url})
             continue
         country_dict = {"country": country.country}
-        country_spidey = SimpleCrawler(url=country.url, disable_crawler_log=True, userland_dict=country_dict)
-        country_spidey.crawl(resource_processor_callback=extract_units_tonals_of_country, crawl_recursively=False)
+        country_spidey_to_extract_units = SimpleCrawler(url=country.url,
+                                                        disable_crawler_log=True,
+                                                        userland_dict=country_dict)
+        country_spidey_to_extract_units.crawl(resource_processor_callback=extract_units_tonals_of_country,
+                                              crawl_recursively=False)
+        if len(country_spidey_to_extract_units.unreachable_child_resources) > 0:
+            INVALID_COUNTRY_HREFS.append({"country": country.country,
+                                         "url": country.url})
 
     for unit in UNIT_COLLECTION:
-        print(unit)
+        print("UCC", len(str(unit)))
+
+    for invalid_country_href in INVALID_COUNTRY_HREFS:
+        print("Not able to reach href `{}` of `{}` in country_spidey".format(
+            invalid_country_href["url"],
+            invalid_country_href["country"]))
 
 
 if __name__ == "__main__":
