f3867c4f891dc2e6ab1b1a0b3a9ea71a73a09276
rnll <neomashab@gmail.com>
2022-12-12 19:22:08 +0530

    Generate id for countries

--- legacyman_parser/parse_countries.py
+++ legacyman_parser/parse_countries.py
@@ -9,28 +9,31 @@ COUNTRY_COLLECTION = []
 
 
 class CountryMap:
-    def __init__(self, country, region, url):
+    def __init__(self, id, country, region, url):
+        self.id = id
         self.country = country
         self.region = region
         self.url = url
 
     def __str__(self):
-        return "{} in {} ==> {}".format(self.country, self.region, self.url)
+        return "{}. {} in {} ==> {}".format(self.id, self.country, self.region.region, self.url)
 
 
 def extract_countries_in_region(soup: BeautifulSoup = None,
                                 parsed_url: str = None,
                                 parent_url: str = None,
                                 userland_dict: dict = None) -> []:
+    seq = len(COUNTRY_COLLECTION)
     for country in soup.find_all('td'):
-        COUNTRY_COLLECTION.append(create_country(country, userland_dict.get("region"), parsed_url))
+        seq = seq + 1
+        COUNTRY_COLLECTION.append(create_country(seq, country, userland_dict['region'], parsed_url))
 
 
-def create_country(country, region, url):
+def create_country(seq, country, region, url):
     url_tag = country.find('a')
     url_str = None
     parsed_url = None
     if url_tag is not None:
         url_str = url_tag.get('href')
         parsed_url = urljoin(url, url_str)
-    return CountryMap(country.getText(), region, parsed_url)
+    return CountryMap(seq, country.getText(), region, parsed_url)
