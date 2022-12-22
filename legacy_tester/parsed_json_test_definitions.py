34ba56226ecd38b0a6b4948eda52640830e09abe
rnll <neomashab@gmail.com>
2022-12-22 22:11:29 +0530

    Replace find method with in operator to search for substring

--- legacy_tester/parsed_json_test_definitions.py
+++ legacy_tester/parsed_json_test_definitions.py
@@ -9,7 +9,7 @@ def phase_one_regions_parse_not_included_countries(published_json, test_payload)
 
 def count_of_tonal_remarks_containing_test_string(published_json, test_payload):
     for unit_payload in test_payload:
-        tonals_with_required_remarks = filter(lambda a: a.remarks.find(unit_payload['test_string']) >= 0,
+        tonals_with_required_remarks = filter(lambda a: unit_payload['test_string'] in a.remarks,
                                               published_json['tonals'])
         if len(list(tonals_with_required_remarks)) != unit_payload['count']:
             print('Error: {} failed to identify {} tonals with "{}" in remarks'.format(unit_payload,
