1fcfb481d97f8c249401f83ae8ad0e10c232a88c
rnll <neomashab@gmail.com>
2023-01-05 19:50:35 +0530

    Use assert to validate assumptions about real and mock data

--- legacyman_parser/parse_abbreviations.py
+++ legacyman_parser/parse_abbreviations.py
@@ -20,8 +20,19 @@ def parse_abbreviations(soup: BeautifulSoup = None,
                     parsed_url: str = None,
                     parent_url: str = None,
                     userland_dict: dict = None) -> []:
+    # Assumption: There's going to be only one table in QuickLinksData/Abbreviations.html
+    assert len(soup.find_all('table')) == 1, "InvalidAssumption: There's going to be only one table in " \
+                                             "QuickLinksData/Abbreviations.html"
+
     compactor = []
     for abbrev_record in soup.find('table').find_all('tr'):
+
+        # Assumption: Each row in this table contains two key-value pairs, side-by-side i.e.
+        # <tr><td>Key1</td><td>Value1</td><td>Key2</td><td>Value2</td></tr>
+        # In other words, row should have only 4 td elements
+        assert len(abbrev_record.find_all('td')) == 4, "InvalidAssumption: Each row in this table contains " \
+                                                       "two key-value pairs, side-by-side"
+
         for abbr_data in abbrev_record.find_all('td'):
             compactor.append(abbr_data.text)
     for idx in range(0, len(compactor)//2):
