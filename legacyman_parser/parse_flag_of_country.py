1e7ae7efbaf1e2b6f80266b97e60e67c69144c31
Ian Mayo <ian@planetmayo.com>
2023-03-31 17:04:55 +0100

    improve flag filtering

--- legacyman_parser/parse_flag_of_country.py
+++ legacyman_parser/parse_flag_of_country.py
@@ -24,24 +24,54 @@ class CountryFlag:
     def __str__(self):
         return "{}'s flag is located at {}.".format(self.country.country, self.file_location)
 
+    ######################################################
+    # filter functions, to help with filtering flag images
+    ######################################################
+
+# filter for images where src is in images sub-folder
+
+
+def in_content_images_filter(tag):
+    src = tag.get('src')
+    return src.lower().startswith('./content/images')
+
+# filter for images that aren't in black-listed images
+
+
+def not_blacklisted_filter(tag):
+    black_list = ['image020.jpg', 'Yellow3.jpg', 'Yellow4.jpg']
+    in_black_list = filter(lambda item: item in tag.get('src'), black_list)
+    in_black_list_item = list(in_black_list)
+    return len(list(in_black_list_item)) == 0
+
 
 def extract_flag_of_country(soup: BeautifulSoup = None, parsed_url: str = None, parent_url: str = None,
                             userland_dict: dict = None) -> []:
     # This parser will handle flag extraction for only non-Britain like countries.
-    flag_images = soup.find_all('img')
+    images = soup.find_all('img')
+
+    # apply our filters
+    in_content_images = list(filter(in_content_images_filter, images))
+    non_blacklisted = list(filter(not_blacklisted_filter, in_content_images))
+    flag_images = non_blacklisted
+
     assert len(flag_images) == 1 or len(flag_images) == 2, "InvalidAssumption: Only 1 or 2 flags " \
                                                            "are there for any given country." \
-                                                           " Failed for {}".format(parsed_url)
+                                                           " Failed for {}. Found {}".format(
+                                                               parsed_url, len(flag_images))
     # Always take the last flag
     flag_image = urljoin(parsed_url, flag_images[len(flag_images) - 1]['src'])
     assert Path(flag_image).is_file(), "InvalidAssumption: Flag file exists if provided in img attribute. " \
-                                       "{} not found as specified in {}.".format(flag_image, parsed_url)
+                                       "{} not found as specified in {}.".format(
+                                           flag_image, parsed_url)
 
     # Copy to target directory
-    destination_file = COPY_FLAGS_TO_DIRECTORY + userland_dict['country'].country + os.path.splitext(flag_image)[1]
+    destination_file = COPY_FLAGS_TO_DIRECTORY + \
+        userland_dict['country'].country + os.path.splitext(flag_image)[1]
     shutil.copy2(flag_image, destination_file)
 
-    COUNTRY_FLAG_COLLECTION.append(CountryFlag(userland_dict['country'], destination_file))
+    COUNTRY_FLAG_COLLECTION.append(CountryFlag(
+        userland_dict['country'], destination_file))
 
 
 def extract_flag_of_ns_country(soup: BeautifulSoup = None, parsed_url: str = None, parent_url: str = None,
@@ -50,17 +80,27 @@ def extract_flag_of_ns_country(soup: BeautifulSoup = None, parsed_url: str = Non
     graylayer = soup.find_all('div', {'id': 'GrayLayer'})
     assert len(graylayer) >= 1, "InvalidAssumption: There should at least one Graylayer div in the landing" \
                                 "page of a non-standard country: " + parsed_url
-    flag_images = graylayer[0].find_all('img')
-    assert len(flag_images) == 1, "InvalidAssumption: There should one image in this Graylayer, " \
-                                  "that of the flag: " + parsed_url
+    images = graylayer[0].find_all('img')
+
+    # apply our filters
+    in_content_images = list(filter(in_content_images_filter, images))
+    non_blacklisted = list(filter(not_blacklisted_filter, in_content_images))
+    flag_images = non_blacklisted
+
+    assert len(flag_images) == 1 or len(flag_images) == 2, "InvalidAssumption: There should one or two images in this Graylayer, " \
+        "that of the flag: {}. Found {}".format(
+        parsed_url, flag_images)
     flag_images = soup.find_all('img')
     # Always take the first flag
     flag_image = urljoin(parsed_url, flag_images[0]['src'])
     assert Path(flag_image).is_file(), "InvalidAssumption: Flag file exists if provided in img attribute. " \
-                                       "{} not found as specified in {}.".format(flag_image, parsed_url)
+                                       "{} not found as specified in {}.".format(
+                                           flag_image, parsed_url)
 
     # Copy to target directory
-    destination_file = COPY_FLAGS_TO_DIRECTORY + userland_dict['country'].country + os.path.splitext(flag_image)[1]
+    destination_file = COPY_FLAGS_TO_DIRECTORY + \
+        userland_dict['country'].country + os.path.splitext(flag_image)[1]
     shutil.copy2(flag_image, destination_file)
 
-    COUNTRY_FLAG_COLLECTION.append(CountryFlag(userland_dict['country'], destination_file))
+    COUNTRY_FLAG_COLLECTION.append(CountryFlag(
+        userland_dict['country'], destination_file))
