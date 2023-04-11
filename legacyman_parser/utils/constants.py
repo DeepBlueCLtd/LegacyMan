c0ab0d43849ab0ffa7a9d159ba3b8c14b2680421
Ian Mayo <ian@planetmayo.com>
2023-04-11 16:34:07 +0100

    use alternate path separator

--- legacyman_parser/utils/constants.py
+++ legacyman_parser/utils/constants.py
@@ -1,8 +1,7 @@
 import os
 from urllib.parse import urljoin
 
-
-TARGET_DIRECTORY = urljoin(os.getcwd()+"/", "target/")
-JSON_EXPORT_FILE = urljoin(TARGET_DIRECTORY, "json_publication.js")
-COPY_FLAGS_TO_DIRECTORY = urljoin(TARGET_DIRECTORY, "flags/")
-COPY_CLASS_IMAGES_TO_DIRECTORY = urljoin(TARGET_DIRECTORY, "class_images/")
+TARGET_DIRECTORY = os.getcwd()+ "/target/"
+JSON_EXPORT_FILE = TARGET_DIRECTORY + "json_publication.js"
+COPY_FLAGS_TO_DIRECTORY = TARGET_DIRECTORY + "flags/"
+COPY_CLASS_IMAGES_TO_DIRECTORY = TARGET_DIRECTORY + "class_images/"
