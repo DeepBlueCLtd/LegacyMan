892c983bbd8c7c494b7412301ba04681333b83ac
Ian Mayo <ian@planetmayo.com>
2023-04-11 15:27:58 +0100

    introduce file separator suffix for flag/image destination

--- legacyman_parser/utils/constants.py
+++ legacyman_parser/utils/constants.py
@@ -4,5 +4,5 @@ from urllib.parse import urljoin
 
 TARGET_DIRECTORY = urljoin(os.getcwd()+"/", "target/")
 JSON_EXPORT_FILE = urljoin(TARGET_DIRECTORY, "json_publication.js")
-COPY_FLAGS_TO_DIRECTORY = urljoin(TARGET_DIRECTORY, "flags")
-COPY_CLASS_IMAGES_TO_DIRECTORY = urljoin(TARGET_DIRECTORY, "class_images")
+COPY_FLAGS_TO_DIRECTORY = urljoin(TARGET_DIRECTORY, "flags/")
+COPY_CLASS_IMAGES_TO_DIRECTORY = urljoin(TARGET_DIRECTORY, "class_images/")
