4b8f982c844bc7881292152a860a2526d48c13dd
Ian Mayo <ian@planetmayo.com>
2023-04-13 14:29:10 +0100

    show warning when tonals table not found

--- legacyman_parser/parse_class_attributes_from_tonals.py
+++ legacyman_parser/parse_class_attributes_from_tonals.py
@@ -30,17 +30,18 @@ def extract_class_attributes_from_tonals_page(soup: BeautifulSoup = None, parsed
         colspan = tdata.get('colspan')
         return colspan and colspan == '4'
     propulsion_system_tables = list(filter(four_col_table_filter, all_tables))
-    assert len(propulsion_system_tables) == 1, "InvalidAssumption: There's always a Tonal Propulsion table with " \
-        "colspan 4 => {}. Found {}".format(
-            parsed_url, len(propulsion_system_tables))
-    propulsion_system_table = propulsion_system_tables[0]
-    assert len(propulsion_system_table) % 2 == 1, "InvalidAssumption: Propulsion table is in" \
-                                                  " Key, Value format, with header : {}".format(
-                                                      parsed_url)
-    propulsion_system_table_data = propulsion_system_table.find_all('td')
-    # Extracting class properties from tonals page
-    userland_dict['class'].power = propulsion_system_table_data[2].text.strip()
-    userland_dict['class'].shaft_blade = propulsion_system_table_data[4].text.strip()
-    userland_dict['class'].bhp = propulsion_system_table_data[10].text.strip()
-    userland_dict['class'].reduction_ratio = propulsion_system_table_data[12].text.strip()
-    userland_dict['class'].av_temp = propulsion_system_table_data[14].text.strip()
+    if len(propulsion_system_tables) != 1:
+        print("Invalid assumption: Tbere is always a table with colspan 4 => {} Found {}" \
+              .format(parsed_url, len(propulsion_system_tables)))
+    else:    
+        propulsion_system_table = propulsion_system_tables[0]
+        assert len(propulsion_system_table) % 2 == 1, "InvalidAssumption: Propulsion table is in" \
+                                                    " Key, Value format, with header : {}".format(
+                                                        parsed_url)
+        propulsion_system_table_data = propulsion_system_table.find_all('td')
+        # Extracting class properties from tonals page
+        userland_dict['class'].power = propulsion_system_table_data[2].text.strip()
+        userland_dict['class'].shaft_blade = propulsion_system_table_data[4].text.strip()
+        userland_dict['class'].bhp = propulsion_system_table_data[10].text.strip()
+        userland_dict['class'].reduction_ratio = propulsion_system_table_data[12].text.strip()
+        userland_dict['class'].av_temp = propulsion_system_table_data[14].text.strip()
