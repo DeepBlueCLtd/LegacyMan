51eb87a3a094fef2bc1f8c76398e9b4adfa37cbd
rnll <neomashab@gmail.com>
2023-04-23 13:42:50 +0530

    Add new python source to implement new attribute parse logic to avoid merge conflicts with brances currently under review

---
+++ legacyman_parser/parse_class_attributes_from_tonals_colspan_insensitive_way.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+
+"""
+Independent testable parse tonal class attributes
+"""
+
+
+def extract_class_attributes_from_tonals_page(soup: BeautifulSoup = None, parsed_url: str = None,
+                                              parent_url: str = None, userland_dict: dict = None) -> []:
+    pass
