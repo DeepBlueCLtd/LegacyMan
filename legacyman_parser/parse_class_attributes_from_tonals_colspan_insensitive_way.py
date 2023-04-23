5e6e7c8215630ece901dea4b88fc7b3c0e03afb3
rnll <neomashab@gmail.com>
2023-04-23 16:45:14 +0530

    Extract attributes in an index-independent manner

--- legacyman_parser/parse_class_attributes_from_tonals_colspan_insensitive_way.py
+++ legacyman_parser/parse_class_attributes_from_tonals_colspan_insensitive_way.py
@@ -46,15 +46,19 @@ def extract_class_attributes_from_tonals_page(soup: BeautifulSoup = None, parsed
                                                "Found {}".format(parsed_url, len(propulsion_system_tables))
 
     propulsion_system_table = propulsion_system_tables[0]
-    propulsion_system_table_data = propulsion_system_table.find_all('td')
     # Extracting class properties from tonals page
-    userland_dict['class'].power = propulsion_system_table_data[2].text.strip()
-    userland_dict['class'].shaft_blade = propulsion_system_table_data[4].text.strip()
-    userland_dict['class'].bhp = propulsion_system_table_data[10].text.strip()
-    userland_dict['class'].reduction_ratio = propulsion_system_table_data[12].text.strip()
-    userland_dict['class'].av_temp = propulsion_system_table_data[14].text.strip()
-    # Extract lookup values
-    # Assign to class
+    # userland_dict['class'].power = propulsion_system_table_data[2].text.strip()
+    # userland_dict['class'].shaft_blade = propulsion_system_table_data[4].text.strip()
+    # userland_dict['class'].bhp = propulsion_system_table_data[10].text.strip()
+    # userland_dict['class'].reduction_ratio = propulsion_system_table_data[12].text.strip()
+    # userland_dict['class'].av_temp = propulsion_system_table_data[14].text.strip()
+    # Extract lookup values and assign to class
+    for html_key, class_attribute in PROPULSION_ATTRIBUTES_HTML_PUB5_MAPPING.items():
+        if class_attribute is None:
+            continue
+        propulsion_system_table_data = propulsion_system_table.find('td', text=html_key)
+        if propulsion_system_table_data is not None:
+            setattr(userland_dict['class'], class_attribute, propulsion_system_table_data.find_next('td').text.strip())
     # Print attributes not found as per lookup
     # TODO A generic attribute parser that can take an expression and assign
     #  (to handle cases where there are two values)
