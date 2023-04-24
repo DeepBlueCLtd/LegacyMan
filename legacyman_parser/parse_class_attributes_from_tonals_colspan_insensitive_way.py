5036879e533ef5cae8a84716330d0e52794e32d6
Ian Mayo <ian@planetmayo.com>
2023-04-24 15:15:12 +0100

    demote assertion to print statement

--- legacyman_parser/parse_class_attributes_from_tonals_colspan_insensitive_way.py
+++ legacyman_parser/parse_class_attributes_from_tonals_colspan_insensitive_way.py
@@ -33,25 +33,26 @@ def extract_class_attributes_from_tonals_page(soup: BeautifulSoup = None, parsed
     propulsion_system_tables = list(
         filter(max_four_column_table_filter_outside_quicklinks, all_tables))
     #   Assert that there's only such table
-    assert len(propulsion_system_tables) == 1, "Invalid assumption: There is always one and only one table " \
-                                               "with 4 columns for propulsion systems => {} " \
-                                               "Found {}".format(parsed_url, len(
-                                                   propulsion_system_tables))
-
-    propulsion_system_table = propulsion_system_tables[0]
-    # Extract lookup values and assign to class
-    for html_key, class_attribute in PROPULSION_ATTRIBUTES_HTML_PUB5_MAPPING.items():
-        if class_attribute is None:
-            continue
-        propulsion_system_table_data = propulsion_system_table.find(
-            'td', text=html_key)
-        if propulsion_system_table_data is not None:
-            setattr(userland_dict['class'], class_attribute,
-                    propulsion_system_table_data.find_next('td').text.strip())
-        # Print attributes not found as per lookup
-        else:
-            print("TONAL ATTRIBUTE PARSE DISCREPANCY: {} not found as tonal attribute in {}".format(
-                html_key, parsed_url))
-    # TODO A generic attribute parser that can take an expression and assign
-    #  (to handle cases where there are two values)
-    # TODO Find all mappings and replace None attributes (possibly as part of #100)
+    if len(propulsion_system_tables) == 1:
+        propulsion_system_table = propulsion_system_tables[0]
+        # Extract lookup values and assign to class
+        for html_key, class_attribute in PROPULSION_ATTRIBUTES_HTML_PUB5_MAPPING.items():
+            if class_attribute is None:
+                continue
+            propulsion_system_table_data = propulsion_system_table.find(
+                'td', text=html_key)
+            if propulsion_system_table_data is not None:
+                setattr(userland_dict['class'], class_attribute,
+                        propulsion_system_table_data.find_next('td').text.strip())
+            # Print attributes not found as per lookup
+            else:
+                print("TONAL ATTRIBUTE PARSE DISCREPANCY: {} not found as tonal attribute in {}".format(
+                    html_key, parsed_url))
+        # TODO A generic attribute parser that can take an expression and assign
+        #  (to handle cases where there are two values)
+        # TODO Find all mappings and replace None attributes (possibly as part of #100)
+    else:
+        print("Invalid assumption: There is always one and only one table "
+              "with 4 columns for propulsion systems => {} "
+              "Found {}".format(parsed_url, len(
+                  propulsion_system_tables)))
