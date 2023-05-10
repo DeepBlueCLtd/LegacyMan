13f17b344d30309230d3863896825fc58de1618c
rnll <neomashab@gmail.com>
2023-05-10 05:56:28 +0530

    Create json_publication inside data subdirectory

--- legacyman_parser/utils/constants.py
+++ legacyman_parser/utils/constants.py
@@ -2,6 +2,6 @@ import os
 from urllib.parse import urljoin
 
 TARGET_DIRECTORY = os.getcwd()+ "/target/"
-JSON_EXPORT_FILE = TARGET_DIRECTORY + "json_publication.js"
+JSON_EXPORT_FILE = TARGET_DIRECTORY + "data/json_publication.js"
 COPY_FLAGS_TO_DIRECTORY = TARGET_DIRECTORY + "flags/"
 COPY_CLASS_IMAGES_TO_DIRECTORY = TARGET_DIRECTORY + "class_images/"
