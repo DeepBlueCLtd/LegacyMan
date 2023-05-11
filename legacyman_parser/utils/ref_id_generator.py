24b25a66af04c26fb354ca65947cd784b80075b9
rnll <neomashab@gmail.com>
2023-05-11 22:15:48 +0530

    Clean up code

--- legacyman_parser/utils/ref_id_generator.py
+++ legacyman_parser/utils/ref_id_generator.py
@@ -1,7 +1,7 @@
 def get_ref_id_for_class(classu):
     # TODO: Introduce string shortening logic
-    return (classu.country.country + ":" \
-            + classu.sub_category[0] + ":" \
+    return (classu.country.country + ":"
+            + classu.sub_category[0] + ":"
             + classu.class_u).lower().replace(" ", "")
 
 
@@ -18,9 +18,9 @@ def tonal_with_seq(tonal_str):
 
 def get_ref_id_for_tonal(tonal):
     # TODO: Introduce string shortening logic
-    return tonal_with_seq((tonal.class_u.country.country + ":" \
-                           + tonal.class_u.sub_category[0] + ":" \
-                           + tonal.class_u.class_u + ":" \
-                           + tonal.tonal_type[0] + ":" \
+    return tonal_with_seq((tonal.class_u.country.country + ":"
+                           + tonal.class_u.sub_category[0] + ":"
+                           + tonal.class_u.class_u + ":"
+                           + tonal.tonal_type[0] + ":"
                            + tonal.source[0]
                            ).lower().replace(" ", ""))
