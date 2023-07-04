c82f55b52fb527275ff94a7488081a6f1e919478
Ian Mayo <ian@planetmayo.com>
2023-07-04 17:36:41 +0100

    correct order of parameters

--- legacyman_parser/parse_non_standard_countries.py
+++ legacyman_parser/parse_non_standard_countries.py
@@ -10,18 +10,26 @@ to process non-standard country data
 """
 NON_STANDARD_COUNTRY_COLLECTION = []
 
-def extract_non_standard_countries_in_region(soup: BeautifulSoup = None,
-                                             parsed_url: str = None,
-                                             parent_url: str = None,
-                                             userland_dict: dict = None) -> []:
-    seq = userland_dict['seq']
-    for area_element in soup.find_all('area'):
-        href = area_element.get('href')
-        country = area_element.get('alt')
-        coords = area_element.get('coords')
-        shape = area_element.get('shape')
-        if href.startswith('..'):
+
+def extract_non_standard_countries_in_region(
+    soup: BeautifulSoup = None,
+    parsed_url: str = None,
+    parent_url: str = None,
+    userland_dict: dict = None,
+) -> []:
+    seq = userland_dict["seq"]
+    for area_element in soup.find_all("area"):
+        href = area_element.get("href")
+        country = area_element.get("alt")
+        coords = area_element.get("coords")
+        shape = area_element.get("shape")
+        if href.startswith(".."):
             parsed_region_url = urljoin(parsed_url, href)
-            NON_STANDARD_COUNTRY_COLLECTION.append(CountryMap(seq.next_value(), country,
-                                                              RegionMap("SPL", country, coords, shape, None),
-                                                              parsed_region_url))
+            NON_STANDARD_COUNTRY_COLLECTION.append(
+                CountryMap(
+                    seq.next_value(),
+                    country,
+                    RegionMap("SPL", country, parsed_region_url, coords, shape),
+                    parsed_region_url,
+                )
+            )
