907ed3976514008ea5dc9a830c0f79dfc13e7b44
Ian Mayo <ian@planetmayo.com>
2023-07-04 16:20:17 +0100

    put data to be shared into composite objects, that get passed by reference, so we can access `live` values

--- test/test_parse_regions.py
+++ test/test_parse_regions.py
@@ -2,7 +2,7 @@ import unittest
 
 from bs4 import BeautifulSoup
 
-from legacyman_parser.parse_regions import extract_regions, REGION_COLLECTION
+from legacyman_parser.parse_regions import extract_regions, REGION_DATA
 
 
 def get_soup(html_input: str):
@@ -13,25 +13,25 @@ class TestParseRegions(unittest.TestCase):
 
     def test_parse_region_all_valid_countries(self):
         print("\nCountries containing no .. at the start: ", end="")
-        REGION_COLLECTION.clear()
+        REGION_DATA.regions.clear()
         soup = get_soup("""<area href="Britain/Britain1.html" />""")
         extract_regions(soup, None, None, None)
-        self.assertEqual(len(REGION_COLLECTION), 1)
+        self.assertEqual(len(REGION_DATA.regions), 1)
 
     def test_parse_region_no_valid_countries(self):
         print("\nCountries containing .. at the start: ", end="")
-        REGION_COLLECTION.clear()
+        REGION_DATA.regions.clear()
         soup = get_soup("""<area href="../Britain/Britain1.html" />""")
         extract_regions(soup, None, None, None)
-        self.assertEqual(len(REGION_COLLECTION), 0)
+        self.assertEqual(len(REGION_DATA.regions), 0)
 
     def test_parse_region_some_valid_countries(self):
         print("\nMix of countries starting and not starting with .. as href: ", end="")
-        REGION_COLLECTION.clear()
+        REGION_DATA.regions.clear()
         soup = get_soup("""<area href="../Britain/Britain1.html" />
                            <area href="Britain/Britain1.html" />""")
         extract_regions(soup, None, None, None)
-        self.assertEqual(len(REGION_COLLECTION), 1)
+        self.assertEqual(len(REGION_DATA.regions), 1)
 
 
 if __name__ == '__main__':
