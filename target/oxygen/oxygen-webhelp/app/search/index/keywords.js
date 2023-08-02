8ea11c2abb1b5af204e9d62c07abaf79ea5d4607
Ian Mayo <ian@planetmayo.com>
2023-08-02 17:24:55 +0100

    introduce output in Oxygen format

---
+++ target/oxygen/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,22 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"britain",p:["p1","p2","p4","p6","p8"]},{w:"-",p:["p2","p4","p6","p8","p10","p11"]},{w:"Complex",p:["p2","p10"]},{w:"Unit_daf",p:["p3"]},{w:"Composite",p:["p4","p11"]},{w:"Unit_ab",p:["p5"]},{w:"Legacy",p:["p6"]},{w:"Unit_da",p:["p7"]},{w:"Standalones",p:["p8"]},{w:"iceland",p:["p9","p10","p11"]},{w:"Unit_ab%TEST-106",p:["p12"]},{w:"Icelandic",p:["p12"]},{w:"Composite%",p:["p12"]},{w:"Unit_g",p:["p13"]},{w:"Regions",p:["p14"]},{w:"Welcome",p:["p15"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[3, 4, 5];
+ph["p3"]=[6];
+ph["p4"]=[3, 4, 7];
+ph["p5"]=[8];
+ph["p6"]=[3, 4, 9];
+ph["p7"]=[10];
+ph["p8"]=[3, 4, 11];
+ph["p9"]=[12];
+ph["p10"]=[12, 4, 5];
+ph["p12"]=[13, 14, 15];
+ph["p11"]=[12, 4, 7];
+ph["p14"]=[17];
+ph["p13"]=[16];
+ph["p15"]=[18];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
