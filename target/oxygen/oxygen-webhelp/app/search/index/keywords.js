4362f61655261e1f7d480b3aba835810bac411e7
Ian Mayo <ian@planetmayo.com>
2023-08-24 10:54:03 +0100

    updated generated content

--- target/oxygen/oxygen-webhelp/app/search/index/keywords.js
+++ target/oxygen/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"britain",p:["p1","p2","p4","p6","p8"]},{w:"-",p:["p2","p4","p6","p8","p10","p11"]},{w:"Complex",p:["p2","p10"]},{w:"Unit_daf",p:["p3"]},{w:"Composite",p:["p4","p11"]},{w:"Unit_ab",p:["p5"]},{w:"Legacy",p:["p6"]},{w:"Unit_1a",p:["p7"]},{w:"Standalones",p:["p8"]},{w:"iceland",p:["p9","p10","p11"]},{w:"Unit_ab%TEST-106",p:["p12"]},{w:"Icelandic",p:["p12"]},{w:"Composite%",p:["p12"]},{w:"Unit_g",p:["p13"]},{w:"Regions",p:["p14"]},{w:"Welcome",p:["p15"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"britain",p:["p1","p2","p4","p6","p9"]},{w:"-",p:["p2","p4","p6","p9","p11","p12"]},{w:"Complex",p:["p2","p11"]},{w:"Unit_daf",p:["p3"]},{w:"Composite",p:["p4","p12"]},{w:"Unit_ab",p:["p5"]},{w:"Legacy",p:["p6"]},{w:"Unit_1a",p:["p7"]},{w:"Unit_2a",p:["p8"]},{w:"Standalones",p:["p9"]},{w:"iceland",p:["p10","p11","p12"]},{w:"Unit_ab%TEST-106",p:["p13"]},{w:"Icelandic",p:["p13"]},{w:"Composite%",p:["p13"]},{w:"Unit_g",p:["p14"]},{w:"Regions",p:["p15"]},{w:"Welcome",p:["p16"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3];
@@ -8,13 +8,14 @@ ph["p4"]=[3, 4, 7];
 ph["p5"]=[8];
 ph["p6"]=[3, 4, 9];
 ph["p7"]=[10];
-ph["p8"]=[3, 4, 11];
-ph["p9"]=[12];
-ph["p10"]=[12, 4, 5];
-ph["p12"]=[13, 14, 15];
-ph["p11"]=[12, 4, 7];
+ph["p8"]=[11];
+ph["p9"]=[3, 4, 12];
+ph["p10"]=[13];
+ph["p12"]=[13, 4, 7];
+ph["p11"]=[13, 4, 5];
 ph["p14"]=[17];
-ph["p13"]=[16];
+ph["p13"]=[14, 15, 16];
+ph["p16"]=[19];
 ph["p15"]=[18];
 var keywordsInfo = {
     keywords: keywords,
--- target/oxygen/oxygen-webhelp/app/search/index/link-to-parent.js
+++ target/oxygen/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {3:-1,18:-1,17:-1};
+var linkToParent = {19:-1,3:-1,18:-1};
