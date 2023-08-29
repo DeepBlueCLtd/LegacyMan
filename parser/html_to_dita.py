e2cd5050fbcd95b981e0309d07fdd7bf609e815d
Ian Mayo <ian@planetmayo.com>
2023-08-29 12:56:38 +0100

    handle spans used for red formatting

--- parser/html_to_dita.py
+++ parser/html_to_dita.py
@@ -182,6 +182,14 @@ def htmlToDITA(file_name, soup_in, dita_soup):
                 for p in bq.find_all("p", recursive=False):
                     p.name = "li"
 
+    # Replace `span` used for red-formatting with a <b> equivalent
+    for span in soup.find_all("span"):
+        if span.has_attr("style"):
+            if "color:#F00" in span["style"]:
+                span.name = "ph"
+                span["outputclass"] = "red"
+                del span["style"]
+
     # for ul in soup.find_all('ul'):
     #     ul.name = 'ol'
 
