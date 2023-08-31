b93964c3cf7665c260e3e6d8e0eeda2c1d7cdaa4
Ian Mayo <ian@planetmayo.com>
2023-08-31 18:12:16 +0100

    update generated content

--- target/oxygen/oxygen-webhelp/app/search/index/keywords.js
+++ target/oxygen/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Britain",p:["p1","p2","p4","p6","p9"]},{w:"-",p:["p2","p4","p6","p9","p11","p12"]},{w:"Complex",p:["p2","p11"]},{w:"Unit_daf",p:["p3"]},{w:"Composite",p:["p4","p12"]},{w:"Unit_ab",p:["p5"]},{w:"Legacy",p:["p6"]},{w:"Unit_1a",p:["p7"]},{w:"Unit_2a",p:["p8"]},{w:"Standalones",p:["p9"]},{w:"Iceland",p:["p10","p11","p12"]},{w:"Unit_ab%TEST-106",p:["p13"]},{w:"Icelandic",p:["p13"]},{w:"Composite%",p:["p13"]},{w:"Unit_g",p:["p14"]},{w:"Regions",p:["p15"]},{w:"Welcome",p:["p16"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Britain",p:["p1","p2","p4","p7","p10"]},{w:"-",p:["p2","p4","p7","p10","p12","p13"]},{w:"Complex",p:["p2","p12"]},{w:"Unit_daf",p:["p3"]},{w:"Composite",p:["p4","p13"]},{w:"Unit_ab",p:["p5"]},{w:"Propulsion",p:["p6"]},{w:"Legacy",p:["p7"]},{w:"Unit_1a",p:["p8"]},{w:"Unit_2a",p:["p9"]},{w:"Standalones",p:["p10"]},{w:"Iceland",p:["p11","p12","p13"]},{w:"Unit_ab%TEST-106",p:["p14"]},{w:"Icelandic",p:["p14"]},{w:"Composite%",p:["p14"]},{w:"Unit_g",p:["p15"]},{w:"Regions",p:["p16"]},{w:"Welcome",p:["p17"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3];
@@ -6,17 +6,18 @@ ph["p2"]=[3, 4, 5];
 ph["p3"]=[6];
 ph["p4"]=[3, 4, 7];
 ph["p5"]=[8];
-ph["p6"]=[3, 4, 9];
-ph["p7"]=[10];
+ph["p6"]=[9];
+ph["p7"]=[3, 4, 10];
 ph["p8"]=[11];
-ph["p9"]=[3, 4, 12];
-ph["p10"]=[13];
-ph["p12"]=[13, 4, 7];
-ph["p11"]=[13, 4, 5];
-ph["p14"]=[17];
-ph["p13"]=[14, 15, 16];
+ph["p9"]=[12];
+ph["p10"]=[3, 4, 13];
+ph["p12"]=[14, 4, 5];
+ph["p11"]=[14];
+ph["p14"]=[15, 16, 17];
+ph["p13"]=[14, 4, 7];
 ph["p16"]=[19];
 ph["p15"]=[18];
+ph["p17"]=[20];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
