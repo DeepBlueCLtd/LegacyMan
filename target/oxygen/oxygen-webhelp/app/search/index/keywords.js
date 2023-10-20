22d967c2ec49db96bb10067c297c03c96b10b3b3
Ian Mayo <ian@planetmayo.com>
2023-10-20 15:27:52 +0100

    Updated release

---
+++ target/oxygen/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,32 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"BR",p:["p1"]},{w:"Complex",p:["p1","p3"]},{w:"-",p:["p1","p3"]},{w:"TRANSDUCERS",p:["p1"]},{w:"Bravo",p:["p2"]},{w:"Transducer",p:["p2","p4","p22","p23"]},{w:"Britain",p:["p3","p11"]},{w:"Echo",p:["p4"]},{w:"Phase",p:["p5","p6","p12","p13"]},{w:"G",p:["p5","p6"]},{w:"UNITS",p:["p5","p12"]},{w:"OVERVIEW",p:["p5","p12"]},{w:"SYSTEMS",p:["p6","p13"]},{w:"Charlie",p:["p7","p8","p22"]},{w:"Pics",p:["p8","p9","p14"]},{w:"banjo",p:["p9","p17"]},{w:"Unit",p:["p10"]},{w:"delta",p:["p10","p23"]},{w:"General",p:["p10"]},{w:"Signature",p:["p10"]},{w:"F",p:["p12","p13"]},{w:"Anchor",p:["p14","p15","p16"]},{w:"I/II",p:["p15"]},{w:"(MTBF)",p:["p15"]},{w:"1",p:["p16"]},{w:"BRUSH",p:["p18"]},{w:"NOISE",p:["p18"]},{w:"ABBREVIATIONS",p:["p19"]},{w:"CONTENTS",p:["p20"]},{w:"INDEX",p:["p20"]},{w:"Quick",p:["p21"]},{w:"Look",p:["p21"]},{w:"Guide",p:["p21"]},{w:"to",p:["p21"]},{w:"vanes",p:["p21"]},{w:"Regions",p:["p24"]},{w:"Welcome",p:["p25"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6];
+ph["p2"]=[7, 8];
+ph["p3"]=[9, 5, 4];
+ph["p4"]=[10, 8];
+ph["p5"]=[11, 12, 13, 14];
+ph["p6"]=[11, 12, 15];
+ph["p7"]=[16];
+ph["p8"]=[16, 17];
+ph["p9"]=[18, 17];
+ph["p10"]=[19, 20, 21, 22];
+ph["p12"]=[11, 23, 13, 14];
+ph["p11"]=[9];
+ph["p14"]=[24, 17];
+ph["p13"]=[11, 23, 15];
+ph["p16"]=[24, 27];
+ph["p15"]=[24, 25, 26];
+ph["p18"]=[28, 29];
+ph["p17"]=[18];
+ph["p19"]=[30];
+ph["p21"]=[33, 34, 35, 36, 37];
+ph["p20"]=[31, 32];
+ph["p23"]=[20, 8];
+ph["p22"]=[16, 8];
+ph["p25"]=[39];
+ph["p24"]=[38];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ target/oxygen/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {36:-1,21:-1,41:-1,40:-1,16:-1};
