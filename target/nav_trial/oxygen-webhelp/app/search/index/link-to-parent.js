5198c5d4cd1c93917416e2f6f31e61a568fb1982
Ian Mayo <ian@planetmayo.com>
2023-11-16 11:31:02 +0000

    Updated release

---
+++ target/nav_trial/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {7:-1,9:-1,11:-1,10:-1,12:-1,8:-1,6:-1};
--- target/oxygen/oxygen-webhelp/app/search/index/link-to-parent.js
+++
@@ -1,2 +0,0 @@
-/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {23:-1,17:-1,93:-1,40:-1,42:-1};
