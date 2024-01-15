7c5e888ef0042fda7aea2c9204a4579e49ede2a0
Robin Wilson <robin@rtwilson.com>
2024-01-15 11:06:55 +0000

    Add file for testing the 'defloating' conversion on a single file

---
+++ parser/run_defloat_conversion.py
@@ -0,0 +1,17 @@
+from lman_parser import Parser
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+p = Parser("", "")
+
+# input_file_path = Path("data/Britain_Cmplx/unit_a28.html")
+# input_file_path = Path("data/France1/FR_A27_Unit.html")
+input_file_path = Path("data/Fr_Legacy/FR_charlie_pics.html")
+html = input_file_path.read_text()
+html_soup = BeautifulSoup(html, "html.parser")
+
+# NEW
+# Deal with the floating tables/images in this document by putting them in the correct chunk of whitespace
+new_html = p.process_floating_parts(html_soup)
+with open("test_converted_output.html", "w") as f:
+    f.write(str(new_html))
